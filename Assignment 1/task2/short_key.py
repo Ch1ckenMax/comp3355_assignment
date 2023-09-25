a05db3dc20888f1297f6552d11ce8afc6e48dc34
Ch1ckenMax <li-hk@hotmail.com>
2023-09-25 17:49:00 +0800

    PA1 task2 fixed

--- Assignment 1/task2/short_key.py
+++ Assignment 1/task2/short_key.py
@@ -8,24 +8,24 @@ def get_factors(N):
     if N % 2 == 0:
         return 2, N // 2
 
-    # From now on, we only need to check odd numbers
+    # From now on, we only need to check odd numbers (any prime number that is larger than 2 is odd)
     potentialPrimeFactor = 3
     primeFactorizationFound = False
+    upperBound = N**(1/2) # Just in case to prevent infinite loop
 
-    while (not primeFactorizationFound) and potentialPrimeFactor <= N**(1/2): 
-        # Check if potentialPrimeFactor is a prime number
-        if not isPrime(potentialPrimeFactor):
-            potentialPrimeFactor = potentialPrimeFactor + 2
-            continue
-        
+    while (not primeFactorizationFound) and potentialPrimeFactor <= upperBound: 
         # Check if the modulus is divisible by the potentialPrimeFactor
+        # Given the assumption that N is a product of two prime numbers, and N has a unique prime factorization. If we found a potentialPrimeFactor such that N % potentialPrimeFactor:
+        #   If potentialPrimeFactor is a prime number, then we found the answer.
+        #   If potentialPrimeFactor is not a prime number:
+        #       We can further do prime factorization to potentialPrimeFactor
+        #       Then, N has a prime factorization that is a product of more than two numbers
+        #       Our assumption states that N has a prime factorization of product of two numbers. Which is a contradiction
+        # Hence, potentialPrimeFactor must be a prime number
         if N % potentialPrimeFactor != 0:
             potentialPrimeFactor = potentialPrimeFactor + 2
-            continue
-
-        # Given the assumption that N is a product of two prime numbers, and N has a unique prime factorization 
-        # Once we found a prime p such that N is divisible by p, p and q = N // potentialPrimeFactor is the prime factorization of N
-        primeFactorizationFound = True
+        else:
+            primeFactorizationFound = True
 
     return potentialPrimeFactor, (N // potentialPrimeFactor)
 
@@ -37,22 +37,19 @@ return d: the decryption exponent
 """
 def get_private_key_from_p_q_e(p, q, e):
     phi = (p - 1) * (q - 1)
-    d = modinv(phi, e)
-    return d
+    k = 0
+
+    while True:
+        term = k * phi + 1
+        d = term // e
+
+        # Check if d satisfies the two conditions
+        if (k * phi + 1) % e == 0 and d != e:
+            return d
+        
+        # Try the next k
+        k = k + 1
 
 def get_student_number():
     # TODO: Fill your student number here
     return "3035745037"
-    
-# Given an integer, determine whether it is a prime number
-def isPrime(number: int) -> bool:
-    if number % 2 == 0:
-        return False
-
-    # We only need to check odd numbers from now on
-    potentialFactor = 3
-    while potentialFactor <= number**(1/2):
-        if number % potentialFactor == 0:
-            return False
-        potentialFactor = potentialFactor + 2
-    return True
